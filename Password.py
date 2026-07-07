e3d610d2efedb79af0944acd80501640c35c490f
Facticee <timon.eliah.plum@gmail.com>
2026-07-07 20:47:24 +0200

    random password

--- Password.py
+++ Password.py
@@ -1,6 +1,12 @@
 from rich.console import Console
+import secrets
+import string
 
 console = Console()
+
+def generate_random_password(pwlength=16):
+    chars = string.ascii_letters + string.digits + "!@#$%^&*()-_=+"
+    return "".join(secrets.choice(chars) for _ in range(pwlength))
 
 def main():
     console.print("[bold cyan]💎 Welcome - Password Vault[/bold cyan]")
@@ -17,6 +23,15 @@ def main():
         if choice == "5":
             print("Programm closed.")
             break
+        elif choice == "4":
+            length = input("Length (Default 16): ")
+
+            if length.isdigit():
+                length = int(length)
+            else:
+                length = 16
+
+            print(generate_random_password(length))
         else:
             print("Test")
 
